431853c2135a0b11191a6cf060d254d7e2deb77a
davidharnisch <davidharnisch@seznam.cz>
2021-11-23 21:54:26 +0100

    2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-
-
-
+(function() {
+    //Select the input element
+    const form = document.querySelector('#message-form')
+    //Set up Submit Button
+    form.addEventListener('submit', function(e){
+        // prevent the form's default submission action
+        e.preventDefault()
+        //Get user's input from from
+       
+     
+    
+     
+    })
+    })()
