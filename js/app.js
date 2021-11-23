576506dc4591643b5a99710f2b2cd6b866a3c46a
davidharnisch <davidharnisch@seznam.cz>
2021-11-23 22:13:40 +0100

    hotovo kromě pokud je pole prázné

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-(function() {
-    //Select the input element
-    const form = document.querySelector('#message-form')
-    //Set up Submit Button
-    form.addEventListener('submit', function(e){
-        // prevent the form's default submission action
-        e.preventDefault()
-        //Get user's input from from
-       
-     
-    
-     
-    })
-    })()
+
+    (function() {
+        //Select the input element
+        const form = document.querySelector('#message-form')
+        //Set up Submit Button
+        form.addEventListener('submit', function(e){
+            // prevent the form's default submission action
+            e.preventDefault()
+            //Get user's input from from
+
+        var inputVal = document.getElementById("message").value;
+        document.querySelector(".message-content").innerHTML = inputVal;
+
+
+        document.getElementById("message-form").reset();
+
+
+
+
+
+//     function getInputValue(){
+//     // Selecting the input element and get its value 
+//     var inputVal = document.getElementById("message").value;
+
+//     console.log(inputVal)
+
+// }           
+         
+        
+         
+        })
+        })()
